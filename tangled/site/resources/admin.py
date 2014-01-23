f8810980f9b9ea1110b2278b80d3d848a3f84206
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-01-22 18:59:47 -0800

    Add /admin/meta page to show settings, etc

--- tangled/site/resources/admin.py
+++ tangled/site/resources/admin.py
@@ -13,3 +13,15 @@ class Users(Resource):
         return {
             'users': users,
         }
+
+
+@config('*/*', permission='sudo')
+class Meta(Resource):
+
+    @config('text/html', template='admin/meta.mako')
+    def GET(self):
+        q = self.request.db_session.query(User)
+        users = q.all()
+        return {
+            'users': users,
+        }
