c025c0991dc490efaa9ee4b9b026d02e8ccb748e
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-06 16:44:41 -0800

    Add tangled to packages list in setup

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author='Wyatt Baldwin',
     author_email='self@wyattbaldwin.com',
     packages=[
+        'tangled',
         'tangled.site',
         'tangled.site.model',
         'tangled.site.resources',
