6fe8388adb188203fd0fc715c95392ec15661a0b
Wyatt Baldwin <self@wyattbaldwin.com>
2017-11-27 18:06:47 -0800

    Replace `req.reset_resource_config()` w/ `del req.resource_config`
    The former was removed from `tangled.web.request.Request` since cached
    properties can now always be safely deleted.

--- tangled/site/resources/home.py
+++ tangled/site/resources/home.py
@@ -13,6 +13,6 @@ class Home(Resource):
             resource = Entry(app, req, urlvars={'id': home_slug})
         else:
             resource = Entries(app, req)
-        req.reset_resource_config()
+        del req.resource_config
         req.resource = resource
         return resource.GET()
